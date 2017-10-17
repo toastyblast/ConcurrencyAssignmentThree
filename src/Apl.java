353c5f42a702c9c9d3d136e2a770ced47f223804
toastyblast <yorankerbusch@gmail.com>
2017-10-17 14:45:51 +0200

    Base first commit

---
+++ src/Apl.java
@@ -0,0 +1,12 @@
+/**
+ * Method that is used to run the simulation of Assignment 3 for Saxion's Concurrency - Method Passing with AKKA.
+ */
+public class Apl {
+    //Variables...
+
+    public static void main(String[] args) {
+        //Code...
+    }
+
+    //Misc. methods...
+}
